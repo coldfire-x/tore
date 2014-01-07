82dbafa87919d6d2bfa085c4074647c450b53a11
Pengfei Xue <pengphy@gmail.com>
2014-01-07 10:57:17 +0800

    add regexp to cleanup html

--- libs/ttr.go
+++ libs/ttr.go
@@ -2,17 +2,52 @@
 
 package libs
 
+import (
+	"regexp"
+	"strings"
+)
+
 type TTR struct {
-	Url, title, text, RawContent string
-	ratio                        map[string]float32
+	Url, title, text, RawContent, cleaned string
+	ratio                                 map[string]float32
 }
 
 // run algorithm
 func (t *TTR) RunAlg() {
+	t.preprocess()
 }
 
 // remove scripts, stylesheets, input, and image
 func (t *TTR) preprocess() {
+	content, err := HTTP.Get(t.Url)
+	if err != nil {
+		panic(err)
+	}
+
+	// copy raw html
+	t.RawContent = content
+
+	// convert html tag to lowercase
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src := re.ReplaceAllStringFunc(content, strings.ToLower)
+
+	// remove style
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	// remove script
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	// remove input
+	re, _ = regexp.Compile("\\<input[\\S\\s]+?\\</input\\>")
+	src = re.ReplaceAllString(src, "")
+
+	// remove continuous line break
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	t.cleaned = src
 }
 
 // count text to tag ratio
@@ -21,12 +56,27 @@ func (t *TTR) countTextToTagRatio() {
 
 // return text
 func (t *TTR) Text() string {
-	return "Text"
+	return t.cleaned
 }
 
 // return title
 func (t *TTR) Title() string {
-	return "Title"
+	if len(t.cleaned) < 1 {
+		return ""
+	}
+
+	re, _ := regexp.Compile("\\<title([\\s\\S]+?)</title\\>")
+	st := re.FindStringSubmatch(t.cleaned)
+	if st == nil {
+		return ""
+	}
+
+	title := st[1]
+	if len(title) > 1 {
+		return title[1:]
+	}
+
+	return ""
 }
 
 func (t *TTR) Init(url string) {
